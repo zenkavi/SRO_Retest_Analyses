5eb02d888201504b06bab71083884cd5d6b52b84
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-07-18 17:14:42 -0700

    cont working on shiny app

--- code/Interactive_Figures/app.R
+++ code/Interactive_Figures/app.R
@@ -1,9 +1,17 @@
+##################################################################
+# LOAD PACKAGES
+##################################################################
+
 library(shiny)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(stringr)
 library(plotly)
+
+##################################################################
+# LOAD LIT REVIEW DATA
+##################################################################
 
 lit_data <- read.csv('../../input/lit_review_figure.csv')
 
@@ -16,32 +24,80 @@ lit_data = lit_data %>%
          days_cutoff = ifelse(days < 60, days, 120),
          days = paste(days, '\n reference:', reference))
 
+##################################################################
+# LOAD BOOT DATA
+##################################################################
+
+retest_data_path = '/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/Retest_03-29-2018/'
+
+process_boot_df = function(df){
+  df = df %>%
+    drop_na() %>%
+    mutate(dv = as.character(dv),
+           icc = as.numeric(as.character(icc)),
+           spearman = as.numeric(as.character(spearman)),
+           pearson = as.numeric(as.character(pearson)),
+           eta_sq = as.numeric(as.character(eta_sq)),
+           sem = as.numeric(as.character(sem)),
+           partial_eta_sq = as.numeric(as.character(partial_eta_sq)),
+           omega_sq = as.numeric(as.character(omega_sq)),
+           var_subs = as.numeric(as.character(var_subs)),
+           var_ind = as.numeric(as.character(var_ind)),
+           var_resid = as.numeric(as.character(var_resid)),
+           F_time = as.numeric(as.character(F_time)),
+           p_time = as.numeric(as.character(p_time)),
+           df_time = as.numeric(as.character(df_time)),
+           df_resid = as.numeric(as.character(df_resid)))
+  return(df)} 
+
+boot_df <- read.csv(gzfile(paste0(retest_data_path,'bootstrap_merged.csv.gz')))
+
+boot_df = process_boot_df(boot_df)
+
+refit_boot_df = read.csv(gzfile(paste0(retest_data_path,'refits_bootstrap_merged.csv.gz')))
+
+refit_boot_df = process_boot_df(refit_boot_df)
+
+fullfit_boot_df = boot_df[as.character(boot_df$dv) %in% unique(as.character(refit_boot_df$dv)),]
+
+boot_df = boot_df[!as.character(boot_df$dv) %in% unique(as.character(refit_boot_df$dv)),]
+
+boot_df = rbind(boot_df, refit_boot_df)
+
+rm(refit_boot_df)
+
+measure_labels <- read.csv('/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/input/measure_labels.csv')
+measure_labels = measure_labels %>% select(-measure_description)
+
+tmp = measure_labels %>%
+  mutate(dv = as.character(dv)) %>%
+  left_join(boot_df[,c("dv", "icc", "spearman")], by = 'dv') 
+
+tmp = tmp %>%
+  separate(dv, c("task_group", "var"), sep="\\.",remove=FALSE,extra="merge") %>%
+  mutate(task_group = factor(task_group, levels = task_group[order(task)])) %>%
+  separate(var, c("var"), sep="\\.",remove=TRUE,extra="drop") %>%
+  mutate(task_group = gsub("_", " ", task_group),
+         var = gsub("_", " ", var)) %>%
+  arrange(task_group, var)
+
+tmp = tmp %>%
+  left_join(rel_df[,c("dv", "icc")], by = "dv") %>%
+  rename(icc = icc.x, point_est = icc.y)
+
+#Manual correction
+tmp = tmp %>%
+  mutate(task = ifelse(task_group == 'holt laury survey', "task", as.character(task))) %>%
+  mutate(task_group = ifelse(task_group == "psychological refractory period two choices", "psychological refractory period", ifelse(task_group == "angling risk task always sunny", "angling risk task",task_group))) %>%
+  mutate(task_group = gsub("survey", "", task_group))
+
+##################################################################
+# UI
+##################################################################
+
 ui <- fluidPage(
   
   titlePanel("Retest reliabilities of self-regulation measures"),
-  
-  # sidebarLayout(
-  #   
-  #   sidebarPanel(
-  #     
-      # selectInput(inputId = "task_type",
-      #             label = "View a task or survey:",
-      #             choices = c("survey", "task")),
-  #     
-      # selectInput(inputId = "task_name",
-      #             label = "Choose task or survey name:",
-      #             choices = c('bis_bas_survey', 'attention_network_task'))
-  #     
-  #   ),
-  #   
-  #   mainPanel(
-  #     
-  #     plotOutput(outputId = "litPlot"),
-  #     
-  #     tableOutput("plotData")
-  #     
-  #   )
-  # )
   
   fluidRow(
     column(12,
@@ -79,6 +135,9 @@ ui <- fluidPage(
   
 )
 
+##################################################################
+# SERVER
+##################################################################
 
 server <- function(input, output) {
   
@@ -117,5 +176,7 @@ server <- function(input, output) {
   
 }
 
-# Create Shiny app ----
+##################################################################
+# SHINY APP
+##################################################################
 shinyApp(ui = ui, server = server)
