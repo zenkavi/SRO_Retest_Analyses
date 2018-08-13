ed37a8ef3d727b9c615dfe9dccbf199fd1c1735e
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-08-13 08:07:51 -0700

    cont refactoring

---
+++ code/SRO_Retest_Helper_Functions/get_numeric_cols.R
@@ -0,0 +1,13 @@
+get_numeric_cols = function(df1 = test_data, df2 = retest_data){
+  
+  numeric_cols = c()
+  
+  for(i in 1:length(names(df1))){
+    if(is.numeric(df1[,i]) & names(df1)[i] %in% names(df2)){
+      numeric_cols <- c(numeric_cols, names(df1)[i])
+    }
+  }
+  
+  return(numeric_cols)
+}
+
