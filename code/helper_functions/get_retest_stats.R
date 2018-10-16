8b0ad3fbd05e6506dfaf83affe35f2f1870777b4
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-10-16 07:10:39 -0700

    refactoring and debugging

--- code/helper_functions/get_retest_stats.R
+++ code/helper_functions/get_retest_stats.R
@@ -28,8 +28,7 @@ get_retest_stats = function(dv_var, t1_df = test_data, t2_df = retest_data, merg
   }
   
   if('icc' %in% metric | 'var_breakdown' %in% metric){
-    df = df %>% select(-dv, -sub_id)
-    icc = ICC(df)
+    icc = ICC(df %>% select(-dv, -sub_id))
     
     if('icc' %in% metric){
       out$icc = icc$results['Average_fixed_raters', 'ICC']
@@ -45,7 +44,7 @@ get_retest_stats = function(dv_var, t1_df = test_data, t2_df = retest_data, merg
   
   if('partial_eta' %in% metric | 'sem' %in% metric){
     
-    mod = summary(aov(score~Error(sub_id)+time, df))
+    mod = summary(aov(score~Error(sub_id)+time, df %>% gather(time, score, -dv, -sub_id)))
     
     if('partial_eta' %in% metric){
       ss_time = as.data.frame(unlist(mod$`Error: Within`))['Sum Sq1',]
