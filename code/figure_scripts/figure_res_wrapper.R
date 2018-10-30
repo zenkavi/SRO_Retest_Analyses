fb92a55abd11074c0bffe844211894827d046e55
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-10-30 11:25:59 -0700

    changed path default

--- code/figure_scripts/figure_res_wrapper.R
+++ code/figure_scripts/figure_res_wrapper.R
@@ -4,7 +4,7 @@ img_dpi <- as.numeric(readline(prompt="Enter img dpi: "))
 img_dpi = ifelse(is.na(img_dpi), 100, img_dpi)
 
 default_fig_path <- readline(prompt="Enter default fig path: ")
-default_fig_path = ifelse(length(default_fig_path)==1, '/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/output/figures/', default_fig_path)
+default_fig_path = ifelse(length(default_fig_path)==1 & exists('fig_path'), fig_path, default_fig_path)
 
 out_device <- readline(prompt="Enter image filetype: ")
 out_device = ifelse(length(out_device)==1, 'jpeg', out_device)
