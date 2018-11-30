8c50295891d0474895d52accc043ef044604e36b
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-11-30 13:06:15 -0800

    added model method to arguments

--- code/helper_functions/sro_predict.R
+++ code/helper_functions/sro_predict.R
@@ -1,9 +1,9 @@
-sro_predict = function(x_df, y_df, cv_folds = 10){
-  
+sro_predict = function(x_df, y_df, cv_folds = 10, m_type = "lm"){
+
   require(tidyverse)
-  
+
   out = data.frame(dv=NA, iv=NA, Rsquared=NA, RsquaredSD=NA)
-  
+
   if("sub_id" %in% names(x_df)){
     x_s = names(x_df)[-which(names(x_df)=="sub_id")]
   }
@@ -15,30 +15,30 @@ sro_predict = function(x_df, y_df, cv_folds = 10){
   }
   else{
     y_s = names(y_df)
-    
+
   }
-  
+
   for(i in y_s){
     for(j in x_s){
-      
+
       x = x_df%>%select(j)
       y = y_df[,i]
-      
+
       print(paste0('Running CV for y= ', i, ' and x= ', j))
-      
+
       model = train(x,y,
-                    method="lm",
+                    method=m_type,
                     trControl = trainControl(method="cv", number=cv_folds),
                     na.action = na.exclude)
-      
+
       tmp = data.frame(dv = i, iv = j, Rsquared = model$results$Rsquared, RsquaredSD = model$results$RsquaredSD)
-      
+
       out = rbind(out, tmp)
-      
+
       print("Done with loop. Saving...")
     }
   }
-  
+
   out = out[-1,]
   return(out)
 }
