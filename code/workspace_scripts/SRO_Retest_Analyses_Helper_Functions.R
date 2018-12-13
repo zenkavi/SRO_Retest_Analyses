27905f3355df8250b60eac5a4bec99d0544e3085
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-12-13 14:51:52 -0800

    working on response to reviewers

--- code/workspace_scripts/SRO_Retest_Analyses_Helper_Functions.R
+++ code/workspace_scripts/SRO_Retest_Analyses_Helper_Functions.R
@@ -6,40 +6,27 @@ library(stringr)
 library(plotly)
 library(DT)
 library(RecordLinkage)
-library(MCMCglmm)
-library(RCurl)
-
-theme_set(theme_bw())
+# library(MCMCglmm)
 
 render_this <- function(){rmarkdown::render('/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/code/SRO_Retest_Analyses.Rmd', output_dir = '/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/output/reports', html_notebook(toc = T, toc_float = T, toc_depth = 2, code_folding = 'hide'))}
 
 options(scipen = 1, digits = 4)
 
-if(from_gh){
-  helper_func_path = 'https://raw.githubusercontent.com/zenkavi/SRO_Retest_Analyses/master/code/helper_functions/'
-  
-  eval(parse(text = getURL(paste0(helper_func_path,'g_legend.R'), ssl.verifypeer = FALSE)))
-  eval(parse(text = getURL(paste0(helper_func_path,'g_caption.R'), ssl.verifypeer = FALSE)))
-  eval(parse(text = getURL(paste0(helper_func_path,'sem.R'), ssl.verifypeer = FALSE)))
-  eval(parse(text = getURL(paste0(helper_func_path,'trim.R'), ssl.verifypeer = FALSE)))
-  eval(parse(text = getURL(paste0(helper_func_path,'get_numeric_cols.R'), ssl.verifypeer = FALSE)))
-  eval(parse(text = getURL(paste0(helper_func_path,'match_t1_t2.R'), ssl.verifypeer = FALSE)))
-  eval(parse(text = getURL(paste0(helper_func_path,'get_retest_stats.R'), ssl.verifypeer = FALSE)))
-  eval(parse(text = getURL(paste0(helper_func_path,'process_boot_df.R'), ssl.verifypeer = FALSE)))
-  eval(parse(text = getURL(paste0(helper_func_path,'make_rel_df.R'), ssl.verifypeer = FALSE)))
+file_names = c('g_legend.R', 'g_caption.R', 'sem.R', 'trim.R', 'get_numeric_cols.R', 'match_t1_t2.R', 'get_retest_stats.R', 'process_boot_df.R', 'make_rel_df.R')
 
+if(from_gh){
+  library(RCurl)
+  helper_func_path = 'https://raw.githubusercontent.com/zenkavi/SRO_Retest_Analyses/master/code/helper_functions/'
+  for(file_name in file_names){
+    eval(parse(text = getURL(paste0(workspace_scripts,file_name), ssl.verifypeer = FALSE)))
+  }
+  
 } else{
   helper_func_path = '/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/code/helper_functions/' 
-  source(paste0(helper_func_path, 'g_legend.R'))
-  source(paste0(helper_func_path, 'g_caption.R'))
-  source(paste0(helper_func_path, 'sem.R'))
-  source(paste0(helper_func_path, 'trim.R'))
-  source(paste0(helper_func_path, 'get_numeric_cols.R'))
-  source(paste0(helper_func_path, 'match_t1_t2.R'))
-  source(paste0(helper_func_path, 'get_retest_stats.R'))
-  source(paste0(helper_func_path, 'process_boot_df.R'))
-  source(paste0(helper_func_path, 'make_rel_df.R'))
+  for(file_name in file_names){
+    source(paste0(workspace_scripts,file_name))
   }
+}
 
 
 
--- code/workspace_scripts/SRO_Retest_Analyses_Workspace.R
+++ code/workspace_scripts/SRO_Retest_Analyses_Workspace.R
@@ -3,34 +3,37 @@
 library(tidyverse)
 library(jsonlite)
 
-workspace_scripts = '/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/code/workspace_scripts/'
-
-source(paste0(workspace_scripts,'SRO_Retest_Analyses_Helper_Functions.R'))
-
-test_data_path = '/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/Complete_03-29-2018/'
-
-retest_data_path = '/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/Retest_03-29-2018/'
-
 fig_path = '/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/output/figures/'
 
-source(paste0(workspace_scripts, 'battery_completion_data.R'))
+cbbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+theme_set(theme_bw())
+ggplot <- function(...) ggplot2::ggplot(...) + scale_fill_manual(values=cbbPalette) + scale_color_manual(values=cbbPalette)+theme(legend.position="bottom")
 
-source(paste0(workspace_scripts,'lit_review_data.R'))
+file_names = c('SRO_Retest_Analyses_Helper_Functions.R', 'battery_completion_data.R', 'lit_review_data.R', 'subject_data.R', 'intratrial_rel_data.R', 'demog_data.R', 'boot_rel_data.R', 'rel_comp_data.R', 'duplicate_items_data.R', 'measure_labels_data.R', 'meaningful_var_data.R', 'bayesian_models_data.R')
 
-source(paste0(workspace_scripts,'subject_data.R'))
+if(from_gh){
+  library(RCurl)
+  
+  workspace_scripts = 'https://raw.githubusercontent.com/zenkavi/SRO_Retest_Analyses/master/code/workspace_scripts/'
+  
+  test_data_path = 'https://raw.githubusercontent.com/zenkavi/Self_Regulation_Ontology/master/Data/Complete_03-29-2018/'
+  
+  retest_data_path = 'https://raw.githubusercontent.com/zenkavi/Self_Regulation_Ontology/master/Data/Retest_03-29-2018/'
+  
+  for(file_name in file_names){
+    eval(parse(text = getURL(paste0(workspace_scripts,file_name), ssl.verifypeer = FALSE)))
+  }
+  
+} else{
+  workspace_scripts = '/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/code/workspace_scripts/'
+  
+  test_data_path = '/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/Complete_03-29-2018/'
+  
+  retest_data_path = '/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/Retest_03-29-2018/'
+  
+  for(file_name in file_names){
+    source(paste0(workspace_scripts,file_name))
+  }
+}
 
-source(paste0(workspace_scripts,'intratrial_rel_data.R'))
 
-source(paste0(workspace_scripts,'demog_data.R'))
-
-source(paste0(workspace_scripts,'boot_rel_data.R'))
-
-source(paste0(workspace_scripts,'rel_comp_data.R'))
-
-source(paste0(workspace_scripts,'duplicate_items_data.R'))
-
-source(paste0(workspace_scripts,'measure_labels_data.R'))
-
-source(paste0(workspace_scripts,'meaningful_vars_data.R'))
-
-source(paste0(workspace_scripts,'bayesian_models_data.R'))
