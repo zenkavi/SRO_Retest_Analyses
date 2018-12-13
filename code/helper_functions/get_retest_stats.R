1d3be874c1a139a23190035713f5dceb8b933b46
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-12-13 12:50:09 -0800

    working on response to reviewers

--- code/helper_functions/get_retest_stats.R
+++ code/helper_functions/get_retest_stats.R
@@ -2,10 +2,14 @@ require(psych)
 require(tidyr)
 
 if(!exists('match_t1_t2')){
-  source('/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/code/helper_functions/match_t1_t2.R')
+  if(from_gh){
+    eval(parse(text = getURL('https://raw.githubusercontent.com/zenkavi/SRO_DDM_Analyses/master/code/helper_functions/match_t1_t2.R', ssl.verifypeer = FALSE)))
+  }else{
+    source('/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/code/helper_functions/match_t1_t2.R')
+  }
 }
 
-get_retest_stats = function(dv_var, t1_df = test_data, t2_df = retest_data, merge_var = 'sub_id', sample='full', sample_vec, metric = c('spearman', 'icc', 'pearson', 'var_breakdown', 'partial_eta', 'sem')){
+get_retest_stats = function(dv_var, t1_df = test_data, t2_df = retest_data, merge_var = 'sub_id', sample='full', sample_vec, metric = c('spearman', 'pearson', 'var_breakdown', 'partial_eta', 'sem', 'icc1.1','icc1.k','icc2.1', 'icc2.k','icc3.1','icc3.k')){
   
   if(sample=='full'){
     df = match_t1_t2(dv_var, t1_df = t1_df, t2_df = t2_df, merge_var = merge_var, format='wide')
@@ -34,11 +38,31 @@ get_retest_stats = function(dv_var, t1_df = test_data, t2_df = retest_data, merg
     out$pearson = cor(df$`1`, df$`2`, method='pearson')
   }
   
-  if('icc' %in% metric | 'var_breakdown' %in% metric){
+  if(sum(c('icc', 'var_breakdown','icc1.1','icc1.k','icc2.1', 'icc2.k','icc3.1','icc3.k') %in% metric) > 0){
     icc = ICC(df %>% select(-dv, -sub_id))
     
-    if('icc' %in% metric){
-      out$icc = icc$results['Average_fixed_raters', 'ICC']
+    if('icc1.1' %in% metric){
+      out$icc1.1 = icc$results['Single_raters_absolute', 'ICC']
+    }
+    
+    if('icc1.k' %in% metric){
+      out$icc1.k = icc$results['Average_raters_absolute', 'ICC']
+    }
+    
+    if('icc2.1' %in% metric){
+      out$icc2.1 = icc$results['Single_random_raters', 'ICC']
+    }
+    
+    if('icc2.k' %in% metric){
+      out$icc2.k = icc$results['Average_random_raters', 'ICC']
+    }
+    
+    if('icc3.1' %in% metric){
+      out$icc3.1 = icc$results['Single_fixed_raters', 'ICC']
+    }
+    
+    if('icc3.k' %in% metric){
+      out$icc3.k = icc$results['Average_fixed_raters', 'ICC']
     }
     
     if('var_breakdown' %in% metric){
