b0810d53a94988ab9638b0e571e8e74f53318869
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-12-17 10:31:08 -0800

    working on response to reviewers

--- code/workspace_scripts/battery_completion_data.R
+++ code/workspace_scripts/battery_completion_data.R
@@ -28,16 +28,16 @@ workers = workers %>%
   mutate(Retest_worker=ifelse(sum(CURRENT.RetestWorker,CURRENT.RetestWorkerB2,CURRENT.RetestWorkerB3,CURRENT.RetestWorkerB4,CURRENT.RetestWorkerB5,na.rm=T)>0,1,0)) %>%
   ungroup()
 
-worker_counts <- fromJSON(paste0(retest_data_path,'/Local/retest_worker_counts.json'))
+worker_counts <- fromJSON('/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/Retest_03-29-2018/Local/retest_worker_counts.json')
 
 worker_counts = as.data.frame(unlist(worker_counts))
 names(worker_counts) = "task_count"
 
-disc_comp_date = read.csv(paste0(retest_data_path,'Local/discovery_completion_dates.csv'), header=FALSE)
-val_comp_date = read.csv(paste0(retest_data_path,'Local/validation_completion_dates.csv'), header=FALSE)
+disc_comp_date = read.csv('/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/Retest_03-29-2018/Local/discovery_completion_dates.csv', header=FALSE)
+val_comp_date = read.csv('/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/Retest_03-29-2018/Local/validation_completion_dates.csv', header=FALSE)
 test_comp_date = rbind(disc_comp_date, val_comp_date)
 rm(disc_comp_date, val_comp_date)
-retest_comp_date = read.csv(paste0(retest_data_path,'Local/retest_completion_dates.csv'), header=FALSE)
+retest_comp_date = read.csv('/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/Retest_03-29-2018/Local/retest_completion_dates.csv', header=FALSE)
 comp_dates = merge(retest_comp_date, test_comp_date, by="V1")
 names(comp_dates) <- c("sub_id", "retest_comp", "test_comp")
 comp_dates$retest_comp = as.Date(comp_dates$retest_comp)
@@ -48,8 +48,8 @@ rm(test_comp_date, retest_comp_date)
 
 ### Completion time data
 
-retest_task_comp_times = read.csv(paste0(retest_data_path, 'Local/retest_task_completion_times.csv'))
-test_task_comp_times = read.csv(paste0(retest_data_path, 'Local/test_task_completion_times.csv'))
+retest_task_comp_times = read.csv('/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/Retest_03-29-2018/Local/retest_task_completion_times.csv')
+test_task_comp_times = read.csv('/Users/zeynepenkavi/Documents/PoldrackLabLocal/Self_Regulation_Ontology/Data/Retest_03-29-2018/Local/test_task_completion_times.csv')
 task_comp_times = merge(retest_task_comp_times, test_task_comp_times, by=c('worker_id','task'))
 rm(retest_task_comp_times, test_task_comp_times)
 task_comp_times = task_comp_times %>%
