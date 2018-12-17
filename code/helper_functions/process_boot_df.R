7304b89f25b608604236764e51b7e2e4bb2cafd3
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-12-17 11:17:17 -0800

    working on response to reviewers

--- code/helper_functions/process_boot_df.R
+++ code/helper_functions/process_boot_df.R
@@ -2,18 +2,13 @@ process_boot_df = function(df){
   df = df %>%
     drop_na() %>%
     mutate(dv = as.character(dv),
-           icc = as.numeric(as.character(icc)),
+           icc2.1 = as.numeric(as.character(icc2.1)),
+           icc3.k = as.numeric(as.character(icc3.k)),
            spearman = as.numeric(as.character(spearman)),
            pearson = as.numeric(as.character(pearson)),
-           eta_sq = as.numeric(as.character(eta_sq)),
            sem = as.numeric(as.character(sem)),
-           partial_eta_sq = as.numeric(as.character(partial_eta_sq)),
-           omega_sq = as.numeric(as.character(omega_sq)),
+           partial_eta = as.numeric(as.character(partial_eta)),
            var_subs = as.numeric(as.character(var_subs)),
            var_ind = as.numeric(as.character(var_ind)),
-           var_resid = as.numeric(as.character(var_resid)),
-           F_time = as.numeric(as.character(F_time)),
-           p_time = as.numeric(as.character(p_time)),
-           df_time = as.numeric(as.character(df_time)),
-           df_resid = as.numeric(as.character(df_resid)))
+           var_resid = as.numeric(as.character(var_resid)))
   return(df)} 
