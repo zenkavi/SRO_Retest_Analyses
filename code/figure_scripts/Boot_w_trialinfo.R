195a93573d3dc17825a04eeeff3c0adc93cbe8df
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-01-30 12:42:42 -0800

    format corrections

--- code/figure_scripts/Boot_w_trialinfo.R
+++ code/figure_scripts/Boot_w_trialinfo.R
@@ -1,6 +1,5 @@
 fig_path = '/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/output/figures/'
 
-
 if(!exists('from_gh')){
   from_gh=FALSE
 }
@@ -23,20 +22,6 @@ if(!exists('boot_df')){
   source('/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/code/workspace_scripts/boot_rel_data.R')
 }
 
-if(!exists('rel_df')){
-  source('/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/code/workspace_scripts/subject_data.R')
-  
-  source('/Users/zeynepenkavi/Dropbox/PoldrackLab/SRO_Retest_Analyses/code/helper_functions/make_rel_df.R')
-  
-  rel_df = make_rel_df(t1_df = test_data, t2_df = retest_data, metrics = c('spearman', 'icc2.1', 'pearson', 'var_breakdown', 'partial_eta', 'sem'))
-  
-  rel_df$task = 'task'
-  rel_df[grep('survey', rel_df$dv), 'task'] = 'survey'
-  rel_df[grep('holt', rel_df$dv), 'task'] = "task"
-  rel_df = rel_df %>%
-    select(dv, task, spearman, icc2.1, pearson, partial_eta, sem, var_subs, var_ind, var_resid)
-}
-
 require(gridExtra)
 require(grid)
 
@@ -48,7 +33,7 @@ meaningful_vars = sort(meaningful_vars)
 
 tmp = measure_labels %>%
   mutate(dv = as.character(dv)) %>%
-  left_join(boot_df[,c("dv", "icc2.1", "spearman")], by = 'dv')
+  left_join(boot_df[,c("dv", "icc2.1")], by = 'dv')
 
 tmp = tmp %>%
   separate(dv, c("task_group", "var"), sep="\\.",remove=FALSE,extra="merge") %>%
@@ -58,26 +43,15 @@ tmp = tmp %>%
          var = gsub("_", " ", var)) %>%
   arrange(task_group, var)
 
-tmp = tmp %>%
-  left_join(rel_df[,c("dv", "icc2.1")], by = "dv") %>%
-  rename(icc2.1 = icc2.1.x, point_est = icc2.1.y)
-
 #Manual correction
 tmp = tmp %>%
   mutate(task = ifelse(task_group == 'holt laury survey', "task", as.character(task))) %>%
   mutate(task_group = gsub("survey", "", task_group),
          task_group = gsub("task", "", task_group),
-         task_group = str_to_title(task_group)) %>%
-  mutate(task_group = ifelse(task_group == "Psychological Refractory Period Two Choices", "PRP", ifelse(task_group == "Angling Risk Always Sunny", "Angling Risk", ifelse(task_group == "Two Stage", "Two Step", ifelse(task_group == "Threebytwo", "Task Switching", ifelse(task_group == "Adaptive N Back", "Adaptive N-back", ifelse(task_group == "Go Nogo", "Go/No-go",  ifelse(task_group == "Ravens", "Raven's", task_group)))))))) %>%
-  mutate(task_group = ifelse(task_group == "Bis Bas ", "BIS-BAS", ifelse(task_group == "Bis11 ", "BIS-11", ifelse(task_group == "Dospert Eb ", "DOSPERT EB", ifelse(task_group == "Dospert Rp ", "DOSPERT RP", ifelse(task_group == "Dospert Rt ", "DOSPERT RT", ifelse(task_group == "Erq ", "ERQ", ifelse(task_group == "Upps Impulsivity ", "UPPS-P", task_group))))))))
-
-lit_review = lit_review %>%
-  mutate(task = ifelse(task_group == 'holt laury survey', "task", as.character(task))) %>%
-  mutate(task_group = gsub("survey", "", task_group),
-         task_group = gsub("task", "", task_group),
-         task_group = str_to_title(task_group)) %>%
-  mutate(task_group = ifelse(task_group == "Psychological Refractory Period Two Choices", "PRP", ifelse(task_group == "Angling Risk Always Sunny", "Angling Risk", ifelse(task_group == "Two Stage", "Two Step", ifelse(task_group == "Threebytwo", "Task Switching", ifelse(task_group == "Adaptive N Back", "Adaptive N-back", ifelse(task_group == "Go Nogo", "Go/No-go",  ifelse(task_group == "Ravens", "Raven's", task_group)))))))) %>%
-  mutate(task_group = ifelse(task_group == "Bis Bas ", "BIS-BAS", ifelse(task_group == "Bis11 ", "BIS-11", ifelse(task_group == "Dospert Eb ", "DOSPERT EB", ifelse(task_group == "Dospert Rp ", "DOSPERT RP", ifelse(task_group == "Dospert Rt ", "DOSPERT RT", ifelse(task_group == "Erq ", "ERQ", ifelse(task_group == "Upps Impulsivity ", "UPPS-P", task_group))))))))
+         task_group = str_to_title(task_group),
+         task_group = gsub(" $","", task_group, perl=T)) %>%
+  mutate(task_group = ifelse(task_group == "Psychological Refractory Period Two Choices", "PRP", ifelse(task_group == "Angling Risk  Always Sunny", "Angling Risk", ifelse(task_group == "Two Stage", "Two Step", ifelse(task_group == "Threebytwo", "Task Switching", ifelse(task_group == "Adaptive N Back", "Adaptive N-back", ifelse(task_group == "Go Nogo", "Go/No-go",  ifelse(task_group == "Ravens", "Raven's", ifelse(task_group == "Columbia Card  Hot", "CCT Hot", ifelse(task_group == "Columbia Card  Cold", "CCT Cold" ,task_group)))))))))) %>%
+  mutate(task_group = ifelse(task_group == "Bis Bas", "BIS-BAS", ifelse(task_group == "Bis11", "BIS-11", ifelse(task_group == "Dospert Eb", "DOSPERT EB", ifelse(task_group == "Dospert Rp", "DOSPERT RP", ifelse(task_group == "Dospert Rt", "DOSPERT RT", ifelse(task_group == "Erq", "ERQ", ifelse(task_group == "Upps Impulsivity", "UPPS-P", ifelse(task_group == "Mindful Attention Awareness", "MAAS",ifelse(task_group == "Mpq Control", "MPQ Control",task_group))))))))))
 
 tmp_mngfl = tmp %>%
   filter(dv %in% meaningful_vars)
@@ -103,7 +77,6 @@ tmp_mngfl = tmp_mngfl %>%
 #Boot plot for tasks with trial info (only using meaningful vars)
 
 p4_t <- tmp_mngfl %>%
-  filter(task == 'task') %>%
   ggplot(aes(x = factor(task_group, levels=rev(unique(task_group))), y = icc2.1)) +
   geom_violin()+
   geom_point(data = lit_review %>% filter(task == 'task'), aes(x = factor(task_group, levels=rev(unique(task_group))), y = retest_reliability), color="#E69F00", fill="#E69F00", size=2, shape=23) +
@@ -138,16 +111,11 @@ task_trial_num_table = task_trial_num %>%
         panel.border = element_blank(),
         axis.text.x = element_text(size=8),
         plot.margin = unit(c(0,-0.25,0,-0.65), "cm"))+
-  # scale_x_discrete(position = "top",
-  #                  breaks=c("mean_num_all_trials", "num_measures"),
-  #                  labels=c("Trials", "Measures"))
   scale_x_discrete(position = "top",
                    breaks=c("mean_num_all_trials"),
                    labels=c("Trials"))
 
 boot_task_plot = arrangeGrob(p4_t, task_trial_num_table, nrow=1, widths = c(4,1), padding = unit(0, "line"))
-
-#ggsave(paste0('Task_Boot_w_trialinfo.', out_device), plot = boot_task_plot, device = out_device, path = fig_path, width = 3.5, height = 8.5, units = "in", limitsize = FALSE, dpi = img_dpi)
 
 #Boot plot for surveys with trial info (only using meaningful vars)
 
@@ -187,16 +155,11 @@ survey_trial_num_table = survey_trial_num %>%
         panel.border = element_blank(),
         axis.text.x = element_text(size=8),
         plot.margin = unit(c(0,-0.25,0,-0.65), "cm"))+
-  # scale_x_discrete(position = "top",
-  #                  breaks=c("mean_num_all_trials", "num_measures"),
-  #                  labels=c("Trials", "Measures"))
     scale_x_discrete(position = "top",
                      breaks=c("mean_num_all_trials"),
                      labels=c("Trials"))
 
 boot_survey_plot = arrangeGrob(p5_t, survey_trial_num_table, nrow=1, widths = c(4,1), padding = unit(0, "line"))
-
-#ggsave(paste0('Survey_Boot_w_trialinfo.', out_device), plot = boot_survey_plot, device = out_device, path = fig_path, width = 3.5, height = 8.5, units = "in", limitsize = FALSE, dpi = img_dpi)
 
 #Both task level boot plots with trial info together
 
--- code/workspace_scripts/lit_review_data.R
+++ code/workspace_scripts/lit_review_data.R
@@ -20,7 +20,8 @@ lit_review = lit_review %>%
   arrange(task_group, raw_fit, var) %>%
   mutate(task_group = gsub("survey", "", task_group),
          task_group = gsub("task", "", task_group),
-         task_group = str_to_title(task_group)) %>%
-  mutate(task_group = ifelse(task_group == "Psychological Refractory Period Two Choices", "Psychological Refractory Period", ifelse(task_group == "Angling Risk Always Sunny", "Angling Risk", ifelse(task_group == "Two Stage", "Two Step", ifelse(task_group == "Threebytwo", "Task Switching", ifelse(task_group == "Adaptive N Back", "Adaptive N-back", ifelse(task_group == "Go Nogo", "Go/No-go",  ifelse(task_group == "Ravens", "Raven's", task_group)))))))) %>%
-  mutate(task_group = ifelse(task_group == "Bis Bas ", "BIS-BAS", ifelse(task_group == "Bis11 ", "BIS-11", ifelse(task_group == "Dospert Eb ", "DOSPERT EB", ifelse(task_group == "Dospert Rp ", "DOSPERT RP", ifelse(task_group == "Dospert Rt ", "DOSPERT RT", ifelse(task_group == "Erq ", "ERQ", ifelse(task_group == "Upps Impulsivity ", "UPPS-P", task_group))))))))  %>%
+         task_group = str_to_title(task_group),
+         task_group = gsub(" $","", task_group, perl=T)) %>%
+  mutate(task_group = ifelse(task_group == "Psychological Refractory Period Two Choices", "PRP", ifelse(task_group == "Angling Risk  Always Sunny", "Angling Risk", ifelse(task_group == "Two Stage", "Two Step", ifelse(task_group == "Threebytwo", "Task Switching", ifelse(task_group == "Adaptive N Back", "Adaptive N-back", ifelse(task_group == "Go Nogo", "Go/No-go",  ifelse(task_group == "Ravens", "Raven's", ifelse(task_group == "Columbia Card  Hot", "CCT Hot", ifelse(task_group == "Columbia Card  Cold", "CCT Cold" ,task_group)))))))))) %>%
+  mutate(task_group = ifelse(task_group == "Bis Bas", "BIS-BAS", ifelse(task_group == "Bis11", "BIS-11", ifelse(task_group == "Dospert Eb", "DOSPERT EB", ifelse(task_group == "Dospert Rp", "DOSPERT RP", ifelse(task_group == "Dospert Rt", "DOSPERT RT", ifelse(task_group == "Erq", "ERQ", ifelse(task_group == "Upps Impulsivity", "UPPS-P", ifelse(task_group == "Mindful Attention Awareness", "MAAS",ifelse(task_group == "Mpq Control", "MPQ Control",task_group)))))))))) %>%
   select(-measure_description)
