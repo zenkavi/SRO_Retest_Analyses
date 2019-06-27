7e1cd8ef114a8e973d18948482156329dbad7137
Zeynep Enkavi <zeynepenkavi@gmail.com>
2019-06-27 11:42:53 -0700

    added verbose option to helper function

--- code/helper_functions/transform_remove_skew.R
+++ code/helper_functions/transform_remove_skew.R
@@ -47,7 +47,7 @@ asin_count <- function(column, tpc){
 #mandatory argument "func" containing function to be applied must be provided
 #optional argument "suffix" containing string to be added to transformed cols can be provided; else default string is .customTr
 
-transform_remove_skew = function(data, columns=get_numeric_cols(df1=data, df2=data), threshold = 1, drop=FALSE, transform='log', ...){
+transform_remove_skew = function(data, columns=get_numeric_cols(df1=data, df2=data), threshold = 1, drop=FALSE, transform='log', verbose=TRUE, ...){
   
   tmp = as.data.frame(apply(data[,columns],2,skew))
   names(tmp) = c("skew")
@@ -67,10 +67,12 @@ transform_remove_skew = function(data, columns=get_numeric_cols(df1=data, df2=da
     names(negative_subset) = tmp$dv[tmp$skew<0]
   }
   
-  cat(rep('*', 40))
-  cat('\n')
-  cat(paste0('Using transformation:', transform))
-  cat('\n')
+  if(verbose){
+    cat(rep('*', 40))
+    cat('\n')
+    cat(paste0('Using transformation:', transform))
+    cat('\n')
+  }
   
   if(exists('positive_subset')){ 
     if(!is.null(ncol(positive_subset)) & ncol(positive_subset)>0){
@@ -107,45 +109,53 @@ transform_remove_skew = function(data, columns=get_numeric_cols(df1=data, df2=da
       successful_transforms = positive_subset[,successful_transforms]
       dropped_vars = names(positive_subset) %w/o% names(successful_transforms)
       
-      cat(rep('*', 40))
-      cat('\n')
-      cat(paste0(length(names(positive_subset)) ,' data positively skewed data were transformed:'))
-      cat('\n')
-      cat(names(positive_subset), sep = '\n')
-      cat(rep('*', 40))
-      cat('\n')
+      if(verbose){
+        cat(rep('*', 40))
+        cat('\n')
+        cat(paste0(length(names(positive_subset)) ,' data positively skewed data were transformed:'))
+        cat('\n')
+        cat(names(positive_subset), sep = '\n')
+        cat(rep('*', 40))
+        cat('\n')
+      }
       
       # replace transformed variables
       data = data[,-c(which(names(data) %in% names(positive_subset)))]
       
       if(drop == TRUE){
         names(successful_transforms) = paste0(names(successful_transforms), suffix)
-        cat(rep('*', 40))
-        cat('\n')
-        cat(paste0('Dropping ', length(dropped_vars) ,' positively skewed data that could not be transformed successfully:'))
-        cat('\n')
-        cat(dropped_vars, sep = '\n')
-        cat(rep('*', 40))
-        cat('\n')
+        if(verbose){
+          cat(rep('*', 40))
+          cat('\n')
+          cat(paste0('Dropping ', length(dropped_vars) ,' positively skewed data that could not be transformed successfully:'))
+          cat('\n')
+          cat(dropped_vars, sep = '\n')
+          cat(rep('*', 40))
+          cat('\n') 
+        }
         data = cbind(data, successful_transforms)
       } else{
         names(positive_subset) = paste0(names(positive_subset), suffix)
-        cat(rep('*', 40))
-        cat('\n')
-        cat(paste0(length(dropped_vars) ,' positively skewed data could not be transformed successfully:'))
-        cat('\n')
-        cat(dropped_vars, sep = '\n')
-        cat(rep('*', 40))
-        cat('\n')
+        if(verbose){
+          cat(rep('*', 40))
+          cat('\n')
+          cat(paste0(length(dropped_vars) ,' positively skewed data could not be transformed successfully:'))
+          cat('\n')
+          cat(dropped_vars, sep = '\n')
+          cat(rep('*', 40))
+          cat('\n') 
+        }
         data = cbind(data, positive_subset)
       }
     } 
   }else{
-    cat(rep('*', 40))
-    cat('\n')
-    cat('No positively skewed variables found.')
-    cat(rep('*', 40))
-    cat('\n')
+   if(versbose){
+     cat(rep('*', 40))
+     cat('\n')
+     cat('No positively skewed variables found.')
+     cat(rep('*', 40))
+     cat('\n')
+   } 
   }
   
   
@@ -183,44 +193,52 @@ transform_remove_skew = function(data, columns=get_numeric_cols(df1=data, df2=da
       successful_transforms = negative_subset[,successful_transforms]
       dropped_vars = names(negative_subset) %w/o% names(successful_transforms)
       
-      cat(rep('*', 40))
-      cat('\n')
-      cat(paste0(length(names(negative_subset)) ,' data negatively skewed data were transformed:'))
-      cat('\n')
-      cat(names(negative_subset), sep = '\n')
-      cat(rep('*', 40))
-      cat('\n')
+      if(verbose){
+        cat(rep('*', 40))
+        cat('\n')
+        cat(paste0(length(names(negative_subset)) ,' data negatively skewed data were transformed:'))
+        cat('\n')
+        cat(names(negative_subset), sep = '\n')
+        cat(rep('*', 40))
+        cat('\n')
+      }
       
       # replace transformed variables
       data = data[,-c(which(names(data) %in% names(negative_subset)))]
       if(drop == TRUE){
         names(successful_transforms) = paste0(names(successful_transforms), suffix)
-        cat(rep('*', 40))
-        cat('\n')
-        cat(paste0('Dropping ', length(dropped_vars) ,' negatively skewed data that could not be transformed successfully:'))
-        cat('\n')
-        cat(dropped_vars, sep = '\n')
-        cat(rep('*', 40))
-        cat('\n')
+        if(verbose){
+          cat(rep('*', 40))
+          cat('\n')
+          cat(paste0('Dropping ', length(dropped_vars) ,' negatively skewed data that could not be transformed successfully:'))
+          cat('\n')
+          cat(dropped_vars, sep = '\n')
+          cat(rep('*', 40))
+          cat('\n') 
+        }
         data = cbind(data, successful_transforms)
       } else{
         names(negative_subset) = paste0(names(negative_subset), suffix)
-        cat(rep('*', 40))
-        cat('\n')
-        cat(paste0(length(dropped_vars) ,' negatively skewed data could not be transformed successfully:'))
-        cat('\n')
-        cat(dropped_vars, sep = '\n')
-        cat(rep('*', 40))
-        cat('\n')
+        if(verbose){
+          cat(rep('*', 40))
+          cat('\n')
+          cat(paste0(length(dropped_vars) ,' negatively skewed data could not be transformed successfully:'))
+          cat('\n')
+          cat(dropped_vars, sep = '\n')
+          cat(rep('*', 40))
+          cat('\n') 
+        }
         data = cbind(data, negative_subset)
       } 
     } 
   } else{
-    cat(rep('*', 40))
-    cat('\n')
-    cat('No negatively skewed variables found.')
-    cat(rep('*', 40))
-    cat('\n')
+    if(verbose){
+      cat(rep('*', 40))
+      cat('\n')
+      cat('No negatively skewed variables found.')
+      cat(rep('*', 40))
+      cat('\n') 
+    }
   }
   
   return(data)
